03aec4eaadbb8ddb46457c87696a832ffdff24e5
Aldo Camacho <aldo.camacho98@gmail.com>
2022-06-19 15:56:32 -0700

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 morgan.token("data", (req, res) => {
@@ -13,6 +14,7 @@ const tinyData =
 
 app.use(express.json());
 app.use(morgan(tinyData));
+app.use(cors());
 
 let persons = [
   {
